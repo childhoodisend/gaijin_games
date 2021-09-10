b65c5a0bcff41f70dc9a8a6a0cb11cb2d7216f91
childhoodisend <morozvktv@gmail.com>
2021-09-10 21:04:17 +0300

    [+] add skeletons

---
+++ server/server.cpp
@@ -0,0 +1,10 @@
+//
+// Created by pahpan on 10.09.2021.
+//
+
+#include "server.h"
+
+int main () {
+
+    return 0;
+}
---
+++ server/server.h
@@ -0,0 +1,14 @@
+//
+// Created by pahpan on 10.09.2021.
+//
+
+#ifndef GAIJIN_GAMES_SERVER_H
+#define GAIJIN_GAMES_SERVER_H
+
+
+class server {
+
+};
+
+
+#endif //GAIJIN_GAMES_SERVER_H
