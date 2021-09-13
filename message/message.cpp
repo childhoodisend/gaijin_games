b37ca8f440c322ee2545ad3367e2e5cdfbd979ef
childhoodisend <morozvktv@gmail.com>
2021-09-13 17:44:34 +0300

    [*] update message.h / .cpp

--- message/message.cpp
+++ message/message.cpp
@@ -1,64 +1,17 @@
-#include <vector>
 #include <iostream>
 
 #include "message.h"
-#include "bson_conv.h"
 
 namespace message_ {
 
-void to_bson(bson_t *b, const message_ptr& msg) {
-
-    if (!BSON_APPEND_UTF8(b, "command", msg->command.c_str())) {
-        throw std::runtime_error("to_bson() err : can't append command ");
-    }
-    if (!BSON_APPEND_UTF8(b, "key", msg->key.c_str())) {
-        throw std::runtime_error("to_bson() err : can't append key ");
-    }
-    if (!BSON_APPEND_UTF8(b, "value", msg->value.c_str())) {
-        throw std::runtime_error("to_bson() err : can't append value ");
-    }
+void request_message::print() const {
+    std::cout << "\"command\" : " << "\"" << command << "\"" << std::endl;
+    std::cout << "\"key\"     : " << "\"" << key     << "\""<< std::endl;
+    std::cout << "\"value\"   : " << "\"" << value   << "\""<< std::endl;
 }
 
-void from_bson(message_ptr& msg_ptr, const bson_t* b) {
-    msg_ptr->command = bson_conv::get_simple_field<std::string>(b, "command", true);
-    msg_ptr->key     = bson_conv::get_simple_field<std::string>(b, "key"    , true);
-    msg_ptr->value   = bson_conv::get_simple_field<std::string>(b, "value"  , true);
-}
-
-void from_bsonbuf(message_ptr& t, const std::vector<uint8_t> &buffer) {
-    bson_t b;
-
-    if(!bson_init_static(&b, buffer.data(), buffer.size())) {
-        throw std::runtime_error("from_bsonbuf() bson init static failed ");
-    }
-    try {
-        from_bson(t, &b);
-    }
-    catch (std::runtime_error &rt) {
-        std::cout << "from_bsonbuf() exc : " << rt.what() << std::endl;
-    }
-}
-
-void to_bsonbuf(std::vector<uint8_t> &dst, const message_ptr& t) {
-
-    dst.clear();
-
-    bson_t b;
-    bson_init(&b);
-    try {
-        to_bson(&b, t);
-    }
-    catch (std::runtime_error &rt) {
-        std::cerr << "to_bsonbuf() exc : " << rt.what() << std::endl;
-    }
-    dst.resize(b.len);
-    memcpy(dst.data(), bson_get_data(&b), b.len);
-}
-
-
-void print(const message_ptr& msg) {
-    std::cout << "\"command\" : " << "\"" << msg->command << "\"" << std::endl;
-    std::cout << "\"key\"     : " << "\"" << msg->key     << "\""<< std::endl;
-    std::cout << "\"value\"   : " << "\"" << msg->value   << "\""<< std::endl;
+void answer_message::print() const{
+    std::cout << "\"key\"     : " << "\"" << key     << "\""<< std::endl;
+    std::cout << "\"value\"   : " << "\"" << value   << "\""<< std::endl;
 }
 }
--- message/message.h
+++ message/message.h
@@ -6,35 +6,106 @@
 #include <string>
 #include <memory>
 #include <utility>
+#include <vector>
 
 #include <bson/bson.h>
+#include "bson_conv.h"
 
 namespace message_ {
-struct message {
+struct request_message {
 public:
-    message(std::string _com, std::string _key, std::string _value) : command(std::move(_com)),
-                                                                      key(std::move(_key)),
-                                                                      value(std::move(_value)) {};
+    request_message() = default;
+    request_message(const std::string& _com,const std::string& _key,const std::string& _value) : command(_com),
+                                                                      key(_key),
+                                                                      value(_value) {};
 
-    ~message() = default;
+    ~request_message() = default;
 
-public:
+    void print() const;
+
     std::string command{};
     std::string key{};
     std::string value{};
 };
 
-typedef std::shared_ptr<message> message_ptr;
+struct answer_message {
+public:
+    answer_message() = default;
+    answer_message(const std::string& _key,const std::string& _value) : key(_key), value(_value) {};
+    ~answer_message() = default;
 
-void from_bson(message_ptr& , const bson_t* );
-void to_bson(bson_t *b, const message_ptr& msg);
+    void print() const;
 
-void from_bsonbuf(message_ptr& t, const std::vector<uint8_t> &buffer);
+    std::string key{};
+    std::string value{};
+};
 
-void to_bsonbuf(std::vector<uint8_t> &dst, const message_ptr& t);
+typedef std::shared_ptr<request_message> request_message_ptr;
+typedef std::shared_ptr<answer_message>  answer_message_ptr;
 
 
-void print(const message_ptr& msg);
+template<typename T>
+inline void to_bson(bson_t *b, const T& msg_ptr);
+
+template<typename T>
+inline void from_bson(T& msg_ptr, const bson_t* b);
+
+template<>
+inline void to_bson<request_message_ptr>(bson_t *b, const request_message_ptr& msg_ptr) {
+    bson_conv::append_simple_field(b, "command", msg_ptr->command);
+    bson_conv::append_simple_field(b, "key", msg_ptr->key);
+    bson_conv::append_simple_field(b, "value", msg_ptr->value);
+}
+template<>
+inline void from_bson<request_message_ptr>(request_message_ptr& msg_ptr, const bson_t* b) {
+    bson_conv::get_simple_field<std::string>(msg_ptr->command, b, "command", true);
+    bson_conv::get_simple_field<std::string>(msg_ptr->key, b, "key"    , true);
+    bson_conv::get_simple_field<std::string>(msg_ptr->value, b, "value"  , true);
+}
+
+template<>
+inline void to_bson<answer_message_ptr>(bson_t *b, const answer_message_ptr& msg_ptr){
+    bson_conv::append_simple_field(b, "key", msg_ptr->key);
+    bson_conv::append_simple_field(b, "value", msg_ptr->value);
+}
+template<>
+inline void from_bson<answer_message_ptr>(answer_message_ptr& msg_ptr, const bson_t* b){
+    bson_conv::get_simple_field<std::string>(msg_ptr->key, b, "key"    , true);
+    bson_conv::get_simple_field<std::string>(msg_ptr->value, b, "value"  , true);
+}
+
+
+
+template<typename T>
+void from_bsonbuf(T& t, const std::vector<uint8_t> &src){
+    bson_t b;
+
+    if(!bson_init_static(&b, src.data(), src.size())) {
+        throw std::runtime_error("from_bsonbuf() bson init static failed ");
+    }
+    try {
+        from_bson(t, &b);
+    }
+    catch (std::runtime_error &rt) {
+        std::cout << "from_bsonbuf() exc : " << rt.what() << std::endl;
+    }
+}
+
+template<typename T>
+void to_bsonbuf(std::vector<uint8_t> &dst, const T& t){
+    dst.clear();
+
+    bson_t b;
+    bson_init(&b);
+    try {
+        to_bson(&b, t);
+    }
+    catch (std::runtime_error &rt) {
+        std::cerr << "to_bsonbuf() exc : " << rt.what() << std::endl;
+    }
+    dst.resize(b.len);
+    memcpy(dst.data(), bson_get_data(&b), b.len);
+}
 
 }
 #endif //GAIJIN_GAMES_MESSAGE_H
